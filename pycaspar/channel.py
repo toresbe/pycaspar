3d7d7758543fe8e8f09ea12c2517a200a40566cc
Tore Sinding Bekkedal <toresbe@gmail.com>
2018-12-10 18:45:32 +0100

    more documentation

--- pycaspar/channel.py
+++ pycaspar/channel.py
@@ -5,6 +5,9 @@ class CasparChannel():
 
     @property
     def framerate(self):
+        """
+        Query server and return the frame rate of the channel.
+        """
         self.caspar._get_info()
         return self.caspar._channels[self.channel_id]['framerate']
 
@@ -14,10 +17,20 @@ class CasparChannel():
 
     @property
     def id(self):
+        """
+        Get channel integer id as used by Caspar internally.
+        """
         return self.channel_id
 
     def layer(self, layer_id):
+        """
+        Get a Caspar Layer object for the given numeric layer ID
+        :param int layer_id: Layer ID
+        """
         return CasparLayer(self.caspar, self.channel_id, layer_id)
 
     def clear(self):
+        """
+        Clear channel and all layers on channel.
+        """
         self.caspar._send_command('CLEAR %s' % (self.name,))
--- pycaspar/layer.py
+++ pycaspar/layer.py
@@ -1,7 +1,6 @@
 class Layer():
     def __init__(self, caspar_instance, channel_id, layer_id):
         """
-
         Create a Caspar Layer object.
 
         :param pycaspar.CasparCG caspar_instance: Caspar instance
@@ -13,19 +12,32 @@ class Layer():
     @property
     def name(self):
         """
-
         Return the Caspar-internal name for the current layer.
         """
         return '%d-%d' % (self.channel_id, self.layer_id)
 
     @property
     def id(self):
+        """
+        Get layer integer id as used by Caspar internally.
+        """
         return self.layer_id
 
     def clear(self):
+        """
+        Clears layer by sending a CLEAR command.
+        """
         self.caspar._send_command('CLEAR %s' % (self.name,))
 
     def play(self, filename, transition = None, loop = False, seek = False):
+        """
+        Play a given resource on the layer.
+
+        :param str filename: Filename or URI to play
+        :param str transition: Transition string (see https://github.com/CasparCG/help/wiki/AMCP-Protocol#loadbg for definition)
+        :param bool loop: Causes the file to play indefinitely.
+        :param int seek: Seek *seek* seconds into file before playing
+        """
         command = 'PLAY ' + self.name + ' '
 
         #FIXME: file name needs proper escaping
